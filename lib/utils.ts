db05bfe24d99a64aecd5ea0aeb6d0a8036b107ca
potma13 <thanaz2@mail.ru>
2026-03-16 20:02:05 +0300

    fix: добавил обработку пустого текста и знаков препинания в truncateText

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,10 +1,16 @@
 export function truncateText(text: string, maxLength: number): string {
+  if (!text) return '';
   if (text.length <= maxLength) return text;
 
-  const truncated = text.substr(0, maxLength);
+  let truncated = text.substr(0, maxLength);
+
   const lastSpace = truncated.lastIndexOf(' ');
 
-  return lastSpace > 0
-    ? truncated.substr(0, lastSpace) + '...'
-    : truncated + '...';
+  if (lastSpace > 0) {
+    truncated = truncated.substr(0, lastSpace);
+  }
+
+  truncated = truncated.replace(/[.,!?;:]$/, '');
+
+  return truncated + '...';
 }
