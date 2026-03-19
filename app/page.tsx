891fc876648efb5edd39a9cb8eadfae1f4e7ce79
potma13 <thanaz2@mail.ru>
2026-03-19 19:15:50 +0300

    style: убрал inline стили

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,8 @@ import { AppTabs } from '@/components/AppTabs';
 
 export default function HomePage() {
   return (
-    <div style={{ backgroundColor: '#F7F7F7', minHeight: '100vh' }}>
-      <main
-        style={{
-          padding: '0px 24px 24px 24px',
-          maxWidth: '1200px',
-          margin: '0 auto',
-          backgroundColor: 'white',
-          minHeight: '100vh',
-        }}
-      >
-        <AppTabs />
-      </main>
-    </div>
+    <main className="container">
+      <AppTabs />
+    </main>
   );
 }
